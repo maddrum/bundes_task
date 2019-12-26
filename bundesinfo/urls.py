3430efd773afe237914608e8875e714ee8f42acb
ico  pld <maddrum9@gmail.com>
2019-12-26 11:35:42 +0200

    added standings

--- bundesinfo/urls.py
+++ bundesinfo/urls.py
@@ -4,5 +4,5 @@ from bundesinfo import views
 app_name = 'bundesinfo'
 
 urlpatterns = [
-
+    url(r'standings/$', views.Standings.as_view(), name='standings')
 ]
--- bundesinfo/views.py
+++ bundesinfo/views.py
@@ -1,5 +1,15 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from bundesinfo.models import TeamStandings
 
 
 class IndexPage(TemplateView):
     template_name = 'bundesinfo/index.html'
+
+
+class Standings(ListView):
+    model = TeamStandings
+    template_name = 'bundesinfo/standings.html'
+
+    def get_queryset(self):
+        qs = TeamStandings.objects.all()
+        return qs
