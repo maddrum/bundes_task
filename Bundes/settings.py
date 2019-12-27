a41762417bc15d5b6a93cafb2a5ca4a40b3298c0
maddrum <maddrum9@gmail.com>
2019-12-27 10:39:08 +0200

    sqlite3 database

--- Bundes/settings.py
+++ Bundes/settings.py
@@ -72,26 +72,26 @@ WSGI_APPLICATION = 'Bundes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'radarstats$default',
-        'USER': 'radarstats',
-        'PASSWORD': 'testtest123',
-        'HOST': 'radarstats.mysql.pythonanywhere-services.com',
-        'PORT': '3306',
-        'OPTIONS': {
-            'sql_mode': 'traditional',
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'radarstats$default',
+#         'USER': 'radarstats',
+#         'PASSWORD': 'testtest123',
+#         'HOST': 'radarstats.mysql.pythonanywhere-services.com',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'sql_mode': 'traditional',
+#         }
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
